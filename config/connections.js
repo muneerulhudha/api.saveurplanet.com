314f9be892173c63e03008a7c4d4baa97daf64fe
Muneer Hudha <muneerulhudha@gmail.com>
2016-11-12 14:25:51 -0600

    Hello Controller

--- config/connections.js
+++ config/connections.js
@@ -56,14 +56,14 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+  mongo: {
+    adapter: 'sails-mongo',
+    host: 'aws-us-east-1-portal.19.dblayer.com',
+    port: 15896,
+    user: 'root', //optional
+    password: 'github123', //optional
+    database: 'saveurplanet' //optional
+  },
 
   /***************************************************************************
   *                                                                          *
